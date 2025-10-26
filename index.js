b4c02dfdd14c481e03efe3a008a7adda813f3814
Salman Sabir <youremail@example.com>
2025-10-26 17:31:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello salman how are u')
+})
+
+app.get('/about', (req, res) => {
+    res.send('salman.com////')
+})
+
+app.get('/login', (req, res) => {
+    res.send('Login Page')
+})
+
+app.get('/youtube',(req, res) => {
+    res.send('<h1>Welcome to YouTube</h1><')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
